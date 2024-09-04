383d8843f465d967fe18ca30f6a24b3e7d89d6fd
Justin Le <justin.le2@mail.mcgill.ca>
2024-09-04 09:16:06 -0400

    Progress on section 4.1 and updates to startup

--- R/4_section/41.R
+++ R/4_section/41.R
@@ -1,3 +1,46 @@
 source("R/utils/startup.R")
 
 # 4.1.1 -------------------------------------------------------------------
+#Vectors for tenure type
+pto_21v <- c("total" = "v_CA21_4288", "owner" = "v_CA21_4305", "tenant" = "v_CA21_4313")
+pto_16v <- c("total" = "v_CA16_4886", "owner" = "v_CA16_4890", "tenant" = "v_CA16_4897")
+pto_11v <- c("total" = "v_CA11N_2277", "owner" = "v_CA11N_2281", "tenant" = "v_CA11N_2288")
+pto_06v <- c("total" = "v_CA06_2048", "owner" = "v_CA06_2053", "tenant" = "v_CA06_2049")
+pto_01v <- c("owner" = "v_CA01_1670", "tenant" = "v_CA01_1666")
+
+#Census Grabbing Function for Laval
+pto_census <- function(datayear, pto_year, cyear){
+  get_census(dataset = datayear,
+             regions = list(CSD = 2465005),
+             level = "CSD",
+             vectors = pto_year
+  ) |> 
+    mutate(Year = cyear) |> 
+    select(-GeoUID, -Type, -`Region Name`, -`Area (sq km)`, -Population,
+           -Dwellings, -Households, -CD_UID, -PR_UID, -CMA_UID)
+}
+
+#Grabbing data for 2001-2021 for Laval
+pto_21 <- pto_census("CA21", pto_21v, "2021")
+pto_16 <- pto_census("CA16", pto_16v, "2016")
+pto_11 <- pto_census("CA11", pto_11v, "2011") |> 
+  select(-`NHS Non Return Rate`)
+pto_06 <- pto_census("CA06", pto_06v, "2006")
+pto_01 <- pto_census("CA01", pto_01v, "2001") |> 
+  mutate(total = owner + tenant)
+
+pto <- bind_rows(pto_21, pto_16, pto_11, pto_06, pto_01) |> 
+  pivot_longer(cols = c("owner", "tenant"), 
+               names_to = "type", 
+               values_to = "count") |> 
+  mutate("count_cc" = convert_number(count),
+         "prop" = count / total,
+         "prop_cc" = convert_pct(count / total))
+
+#Creating a grouped bar chart
+ggplot(pto, aes(x = Year, y = count, fill = type)) +
+  geom_bar(stat = "identity", position = "dodge") +
+  scale_fill_manual(values = c("owner" = "#A3B0D1", "tenant" = "#CD718C")) +
+  scale_y_continuous(labels = function(x) convert_number(x)) +
+  labs(x = "", y = "Nombre de ménages", fill = "Year") +
+  graph_theme
--- R/utils/startup.R
+++ R/utils/startup.R
@@ -1,3 +1,61 @@
+# Loading Libraries -------------------------------------------------------
 library(cmhc)
 library(cancensus)
 library(tidyverse)
+library(curbcut)
+library(extrafont)
+library(showtext)
+library(sf)
+library(gt)
+
+# Checking vectors for Canada ---------------------------------------------
+cmhc_breakdown <- list_cmhc_breakdowns()
+cancensus_21_vectors <- list_census_vectors("CA21")
+
+# Addition of colors ------------------------------------------------------
+
+curbcut_colors <- cc.buildr::build_colours()
+
+# Colors from brandbook
+curbcut_colors$brandbook <- 
+  tibble::tibble(theme = c("greydata", "blueexplorer", "greenecology", "redhousing",
+                           "pinkhealth", "purpletransport", "yellowclimate",
+                           "greenurbanlife", "browndemographics"),
+                 color = c("#F0F0F0", "#A3B0D1", "#73AD80", "#E08565", "#CD718C", 
+                           "#C9C3FA", "#F5D574", "#ADB033", "#9E9090"))
+
+# Themes ------------------------------------------------------------------
+
+graph_theme <- theme_minimal() +
+  theme(legend.title = element_blank(), legend.position = "bottom",
+        text=element_text(family="KMR Apparat Regular"))
+
+# Number functions --------------------------------------------------------
+convert_hundreds <- function(x) {
+  curbcut:::round_big_marks(
+    x = x,
+    min_dig = 5,
+    scale_fun = scales::comma
+  )
+}
+
+convert_number <- function(x) {
+  x[!is.na(x)] <- curbcut::convert_unit(x = x[!is.na(x)])
+  gsub(",", " ", x)
+}
+
+convert_number_tens <- \(x) convert_number(round(x / 10) * 10)
+
+convert_number_noround <- function(x) {
+  scales::comma(x, 1, accuracy = 1, big.mark = " ")
+}
+
+convert_pct <- function(x) {
+  out <- curbcut:::convert_unit.pct(x = x, decimal = 1)
+  out <- gsub("\\.", ",", out)
+  out <- gsub("\\%", " %", out)
+  for (i in seq_along(out)) {
+    if (!grepl("\\,", out[[i]])) out[[i]] <- gsub(" %", ",0 %", out[[i]])
+  }
+  out
+}
