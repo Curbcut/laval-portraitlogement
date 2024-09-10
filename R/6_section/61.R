74fd73b4bc238fd551f1ef018ffab36b6e4f1236
dwachsmuth <david.wachsmuth@mcgill.ca>
2024-09-10 12:59:58 -0400

    6.1

--- R/6_section/61.R
+++ R/6_section/61.R
@@ -1,1 +1,18 @@
-source("R/startup.R")
+#### 6.1 #######################################################################
+
+source("R/utils/startup.R")
+
+
+# 6.1.2. Mises en chantier par typologie ----------------------------------
+
+starts_by_type <- 
+  map(2010:2023, \(x) {
+    get_cmhc(
+      survey = "Scss",
+      series = "Starts", 
+      dimension = "Dwelling Type",
+      breakdown = "Survey Zones", 
+      geo_uid = "2465005",
+      year = x)}) |> 
+  bind_rows()
+
