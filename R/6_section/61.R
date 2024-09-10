54f148e43d1799b22b63d748e62ae3102feae9d1
dwachsmuth <david.wachsmuth@mcgill.ca>
2024-09-10 13:06:59 -0400

    6.1 update

--- R/6_section/61.R
+++ R/6_section/61.R
@@ -2,8 +2,13 @@
 
 source("R/utils/startup.R")
 
+list_cmhc_dimensions("Scss", "Starts")
 
-# 6.1.2. Mises en chantier par typologie ----------------------------------
+
+# 6.1.1 Dév rés récent (répartition territoriale par type de logem --------
+
+
+# 6.1.2 Mises en chantier par typologie -----------------------------------
 
 starts_by_type <- 
   map(2010:2023, \(x) {
@@ -16,3 +21,16 @@ starts_by_type <-
       year = x)}) |> 
   bind_rows()
 
+
+# 6.1.3 Mises en chantier par mode d'occupation ---------------------------
+
+starts_by_market <- 
+  map(2010:2023, \(x) {
+    get_cmhc(
+      survey = "Scss",
+      series = "Starts", 
+      dimension = "Intended Market",
+      breakdown = "Survey Zones", 
+      geo_uid = "2465005",
+      year = x)}) |> 
+  bind_rows()
