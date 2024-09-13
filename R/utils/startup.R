6462447a40b29f105af948a06fb097ecd66569bb
dwachsmuth <david.wachsmuth@mcgill.ca>
2024-09-13 08:39:21 -0400

    Startup script cleanup

--- R/utils/startup.R
+++ R/utils/startup.R
@@ -1,4 +1,7 @@
+#### STARTUP SCRIPT ############################################################
+
 # Loading Libraries -------------------------------------------------------
+
 library(cmhc)
 library(cancensus)
 library(scales)
@@ -11,11 +14,15 @@ library(sf)
 library(gt)
 library(readxl)
 
+
 # Checking vectors for Canada ---------------------------------------------
+
 cmhc_breakdown <- list_cmhc_breakdowns()
 cancensus_21_vectors <- list_census_vectors("CA21")
 
+
 # Shapefiles -------------------------------------------------------------
+
 laval_da <- get_census(dataset = "CA21",
                        regions = list(CSD = 2465005),
                        level = "DA",
@@ -30,35 +37,41 @@ laval_ct <- get_census(dataset = "CA21",
   select(GeoUID) |> 
   st_transform(crs = 4326)
 
-# Addition of colors ------------------------------------------------------
+
+# Addition of colours -----------------------------------------------------
 
 curbcut_colors <- cc.buildr::build_colours()
 curbcut_scale <- c("#C4CDE1", "#98A8CB", "#6C83B5", "#4C5C7F", "#2B3448")
 
-# Colors from brandbook
-curbcut_colors$brandbook <- 
-  tibble::tibble(theme = c("greydata", "blueexplorer", "greenecology", "redhousing",
-                           "pinkhealth", "purpletransport", "yellowclimate",
-                           "greenurbanlife", "browndemographics"),
-                 color = c("#F0F0F0", "#A3B0D1", "#73AD80", "#E08565", "#CD718C", 
-                           "#C9C3FA", "#F5D574", "#ADB033", "#9E9090"))
+# Colours from brandbook
+curbcut_colors$brandbook <- tibble(
+  theme = c("greydata", "blueexplorer", "greenecology", "redhousing", 
+            "pinkhealth", "purpletransport", "yellowclimate",
+            "greenurbanlife", "browndemographics"),
+  color = c("#F0F0F0", "#A3B0D1", "#73AD80", "#E08565", "#CD718C", 
+            "#C9C3FA", "#F5D574", "#ADB033", "#9E9090"))
+
 
 # Themes ------------------------------------------------------------------
 
-graph_theme <- theme_minimal() +
+graph_theme <- 
+  theme_minimal() +
   theme(legend.title = element_blank(), legend.position = "bottom",
         legend.margin = margin(t = -5, r = 0, b = 5, l = 0),
         text=element_text(family="KMR Apparat Regular"),
         legend.text = element_text(size = 10),
         axis.title.y = element_text(size = 11))
 
+
 # Map Theme ---------------------------------------------------------------
 
-lvl <- cancensus::get_census("CA21", regions = list(CSD = 2465005), 
-                             level = "CSD",
-                             geo_format = "sf") |> 
+lvl <- 
+  get_census("CA21", regions = list(CSD = 2465005), level = "CSD", 
+             geo_format = "sf") |> 
   st_transform(crs = 32618)
+
 lvlbbox <- sf::st_bbox(lvl)
+
 laval_sectors <- qs::qread("data/geom_context/secteur.qs")
 
 tiles <- mapboxapi::get_static_tiles(
@@ -66,21 +79,24 @@ tiles <- mapboxapi::get_static_tiles(
   username = "curbcut",
   zoom = 11,##
   style_id = "clz1cxvvi021f01nxef327y5p",#"cljkciic3002h01qveq5z1wrp",
-  access_token = "pk.eyJ1IjoiY3VyYmN1dCIsImEiOiJjbGprYnVwOTQwaDAzM2xwaWdjbTB6bzdlIn0.Ks1cOI6v2i8jiIjk38s_kg",
+  access_token = paste0("pk.eyJ1IjoiY3VyYmN1dCIsImEiOiJjbGprYnVwOTQwaDAz",
+                        "M2xwaWdjbTB6bzdlIn0.Ks1cOI6v2i8jiIjk38s_kg"),
   scaling_factor = "2x")
 
 gg_cc_tiles <- list(ggspatial::layer_spatial(tiles, alpha = 0.7))
-default_theme <- theme(legend.position = "bottom",
-                       legend.box = "vertical",
-                       legend.title = element_text(size = 11, family="KMR Apparat Regular"),
-                       legend.text = element_text(size = 10, family="KMR Apparat Regular"),
-                       legend.title.align = 0.5,
-                       legend.text.align = 0.5,
-                       text=element_text(size = 10, family="KMR Apparat Regular"), 
-                       legend.box.margin = margin(t = -10))
+
+default_theme <- theme(
+  legend.position = "bottom",
+  legend.box = "vertical",
+  legend.title = element_text(size = 11, family="KMR Apparat Regular"),
+  legend.text = element_text(size = 10, family="KMR Apparat Regular"),
+  legend.title.align = 0.5,
+  legend.text.align = 0.5,
+  text=element_text(size = 10, family="KMR Apparat Regular"), 
+  legend.box.margin = margin(t = -10))
 
 #Converting the bounding box to be compatible with EPSG:4326
-bbox <- data.frame(
+bbox <- tibble(
   x = c(lvlbbox["xmin"], lvlbbox["xmax"]),
   y = c(lvlbbox["ymin"], lvlbbox["ymax"])) |> 
   st_as_sf(coords = c("x", "y"), crs = 32618) |> 
@@ -113,7 +129,9 @@ gg_cc_theme <- c(list(
   list(theme(legend.box.margin = margin(t = 0)))
 )
 
+
 # Number functions --------------------------------------------------------
+
 convert_hundreds <- function(x) {
   curbcut:::round_big_marks(
     x = x,
